fbadf9c91576255dcb11bd149bd35f0a189b657e
Antti Keränen <detegr@rbx.email>
2025-01-01 22:43:41 +0200

    Add a way to specify if FF shaders are multiview

--- src/d3d9/d3d9_device.h
+++ src/d3d9/d3d9_device.h
@@ -1056,12 +1056,14 @@ namespace dxvk {
 
     HRESULT CreateRenderTargetFromDesc(D3D9_COMMON_TEXTURE_DESC* pDesc, IDirect3DSurface9** ppSurface, HANDLE* pSharedHandle);
 
-  private:
-
     DxvkCsChunkRef AllocCsChunk() {
       DxvkCsChunk* chunk = m_csChunkPool.allocChunk(DxvkCsChunkFlag::SingleUse);
       return DxvkCsChunkRef(chunk, &m_csChunkPool);
     }
+
+    bool MultiViewFF() const { return m_multiViewFF; }
+
+  private:
 
     template<bool AllowFlush = true, typename Cmd>
     void EmitCs(Cmd&& command) {
@@ -1484,6 +1486,7 @@ namespace dxvk {
     D3D9VkInteropDevice             m_d3d9Interop;
     D3D9On12                        m_d3d9On12;
     DxvkD3D8Bridge                  m_d3d8Bridge;
+    bool                            m_multiViewFF;
   };
 
 }
